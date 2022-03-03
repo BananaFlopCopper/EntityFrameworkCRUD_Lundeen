0e8eafdf3953fd236929919e3fd656c76f05d8d8
CopperBanana <aiden.lundeen@student.centralia.edu>
2022-03-02 17:48:25 -0800

    Add project files.

---
+++ EntityFrameworkCRUD_Lundeen/Context/SalesContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using EntityFrameworkCRUD_Lundeen.Models;
+
+namespace EntityFrameworkCRUD_Lundeen.Context
+{
+    public partial class SalesContext : DbContext
+    {
+        public static IConfiguration Config; 
+        public SalesContext()
+        { 
+        }
+        
+        public SalesContext(DbContextOptions<SalesContext> options, IConfiguration conf)
+            : base(options)
+        {
+            Config = conf;
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = Config.GetConnectionString("Conn");
+                optionsBuilder.UseSqlServer(conn);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.City).HasMaxLength(40);
+
+                entity.Property(e => e.Country).HasMaxLength(40);
+
+                entity.Property(e => e.FirstName).HasMaxLength(40);
+
+                entity.Property(e => e.LastName).HasMaxLength(40);
+
+                entity.Property(e => e.Phone).HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EntityFrameworkCRUD_Lundeen/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkCRUD_Lundeen.Models
+{
+    public partial class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? Phone { get; set; }
+    }
+}
